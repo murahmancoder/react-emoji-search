973eb5c253154de6ae16929c32bd4e1f43823bb0
Muhammad Rahman <rahman.muraman@gmail.com>
2021-08-12 15:45:56 +0700

    membuat data fetching direact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [emojisData, setEmojisData] = useState([]);
+
+  useEffect(() => {
+    // callback
+    async function fetchEmojis() {
+      const res = await axios.get(
+        "https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28"
+      );
+      console.log(res);
+    }
+
+    // pemanggilan atau menjalankan function
+    fetchEmojis();
+  }, []);
+
+  return <div className="App">Hello WOrld</div>;
 }
 
 export default App;
