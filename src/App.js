fbcc3b0b1a916f4e2aea2d32a2e0ad228c7ec817
Muhammad Rahman <rahman.muraman@gmail.com>
2021-08-12 16:06:15 +0700

    store respons data ke state

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ import "./App.css";
 
 function App() {
   const [emojisData, setEmojisData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     // callback
     async function fetchEmojis() {
-      const res = await axios.get(
-        "https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28"
-      );
-      console.log(res);
+      // loading dulu terjadi berarti ubah jadi true
+      setLoading(true);
+
+      // error handling
+      try {
+        const res = await axios.get(
+          "https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28"
+        );
+        // menyimpan data res emoji data ke state
+        setEmojisData(res.data);
+
+        // proses selesai loading jadi false
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+
+        // seterror dan loading jadi false
+        setError(true);
+        setLoading(true);
+      }
+      // console.log(res);
     }
 
     // pemanggilan atau menjalankan function
     fetchEmojis();
   }, []);
+
+  // console.log("loading", loading);
+  // console.log("error", error);
+  // console.log("res", emojisData);
 
   return <div className="App">Hello WOrld</div>;
 }
